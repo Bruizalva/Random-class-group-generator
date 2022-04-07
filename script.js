66b779236ed104158aac5afd6b5f3ef45957e54d
A96B02028B <brian_coyote@hotmail.com>
2022-04-07 15:24:28 +0100

    Ahora se muestra el grupo de clase y se puede añadir alumnos

--- script.js
+++ script.js
@@ -3,34 +3,84 @@ var groupSize = groupSizeInput.value;
 
 const DOM = {
     outputSection: document.querySelector("#outputSection"),
+    studentInput: document.querySelector("#studentInput"),
+    addStudentBtn: document.querySelector("#addStudentBtn"),
     generateGroupsBtn: document.querySelector("#generateGroupsBtn"),
     groupSizeInput: document.querySelector("#groupSizeInput"),
-    classGroups: document.querySelector("#classGroups"),
+    teams: document.querySelector("#teams"),
+    classGroup: document.querySelector("#classGroup"),
 }
 
-DOM.generateGroupsBtn.addEventListener("click", showGroups);
+DOM.groupSizeInput.addEventListener("input", function(){groupSize = groupSizeInput.value});
+DOM.generateGroupsBtn.addEventListener("click", showTeams);
+DOM.addStudentBtn.addEventListener("click", addStudent);
 
 
-function showGroups()
+(function()
 {
-    let groups = makeGroups(students, 4);
-    let groupId = 1;
-    groups.forEach(group =>
+    showStudents();
+})();
+
+
+/**
+ * Añade un nuevo estudiante al grupo de clase actual.
+ */
+function addStudent()
+{
+    let newStudent = DOM.studentInput.value.trim();
+    if (newStudent.length > 1)
     {
-        let groupDiv = document.createElement("div");
-        groupDiv.classList.add("classGroup");
-        let groupIdHeader = document.createElement("h3");
-        groupIdHeader.textContent = "Grupo " + groupId++;
-        groupDiv.append(groupIdHeader);
-        group.forEach(student => 
+        students.push(newStudent);
+        showStudents();
+    }
+}
+
+
+/**
+ * Muestra en pantalla a los alumnos que forman el grupo de clase actual.
+ */
+function showStudents()
+{
+    DOM.classGroup.innerHTML = "";
+    students.forEach(student => {
+        let studentDiv = document.createElement("div");
+        studentDiv.classList.add("student");
+        let studentName = document.createElement("p");
+        studentName.textContent = student;
+        studentDiv.append(studentName);
+        DOM.classGroup.append(studentDiv);
+    });
+}
+
+
+/**
+ * Genera equipos y los muestra en la sección teams.
+ */
+function showTeams()
+{
+    if (DOM.groupSizeInput.validity.valid)
+    {
+        let generatedTeams = makeGroups(students, groupSize);
+        let teamId = 1;
+        DOM.teams.innerHTML = "";
+        generatedTeams.forEach(team =>
         {
-            studentP = document.createElement("p");
-            studentP.classList.add("student");
-            studentP.textContent = student;
-            groupDiv.append(studentP);
-        })
-        DOM.classGroups.append(groupDiv);
-    })
+            let teamDiv = document.createElement("div");
+            teamDiv.classList.add("team");
+            let teamIdHeader = document.createElement("h3");
+            teamIdHeader.textContent = "Grupo " + teamId++;
+            teamDiv.append(teamIdHeader);
+            team.forEach(teamMember => 
+            {
+                teamMemberP = document.createElement("p");
+                teamMemberP.classList.add("teamMember");
+                teamMemberP.textContent = teamMember;
+                teamDiv.append(teamMemberP);
+            })
+            DOM.teams.append(teamDiv);
+        });
+    }
+    
 }
 
 
